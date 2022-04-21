108357eb034ebb3ce5a1a9b2f0f6eaac5bd832ef
Cristian Vianey Cáceres Labrador <80643642+cristiancacereslabrador@users.noreply.github.com>
2022-04-21 10:57:51 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,86 @@
+import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+import Error from "./Error";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+
+const Formulario = ({ setMonedas }) => {
+  const [criptos, setCriptos] = useState([]);
+  const [error, setError] = useState(false);
+
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu Moneda", monedas);
+  const [criptomoneda, SelectCriptomoneda] = useSelectMonedas(
+    "Elige tu Criptomoneda",
+    criptos
+  );
+
+  // const [SelectCriptomonedas] = useSelectMonedas('Elige tu Criptomoneda')
+  // SelectMonedas()
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url =
+        "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      // console.log(resultado.data);
+
+      const arrayCryptos = resultado.Data.map((cripto) => {
+        // console.log(cripto.CoinInfo.FullName)
+        const objeto = {
+          id: cripto.CoinInfo.Name,
+          nombre: cripto.CoinInfo.FullName,
+        };
+        // console.log(objeto)
+        return objeto;
+      });
+      // console.log(arrayCryptos)
+      setCriptos(arrayCryptos);
+    };
+    consultarAPI();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if ([moneda, criptomoneda].includes("")) {
+      // console.log('ERROR');
+      setError(true);
+      return;
+    }
+    setError(false);
+    setMonedas({ moneda, criptomoneda });
+  }
+
+  return (
+    <>
+      {error && <Error>Todos los campos son obligatorios</Error>}
+      <form onSubmit={handleSubmit}>
+        <SelectMonedas />
+        <SelectCriptomoneda />
+
+        <InputSubmit type="submit" value="Cotizar" />
+      </form>
+    </>
+  );
+};
+
+export default Formulario;
